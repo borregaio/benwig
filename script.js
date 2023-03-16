ad371669bb87273d4dc1b828a5bd39f1fc411a33
Carlos Borrega <c.borrega@proton.me>
2023-03-16 19:39:43 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggle() {
+    var click = document.getElementById("projects");
+    if (click.style.display === "none") {
+       click.style.display = "block";
+    } else {
+       click.style.display = "none";
+    }
+ }
+
+ function notDoneYet() {
+    alert("Not done yet!");
+ }
