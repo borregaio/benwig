eea9f9eecd989b490bf0ed24c047c9a4490f39c3
Carlos Borrega <c.borrega@proton.me>
2023-07-04 14:15:06 +0100

    fixed modal

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 // Get the modal
 var modal = document.getElementById("myModal");
 
+
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var images = document.getElementsByClassName("myImages");
 var modalImg = document.getElementById("img01");
@@ -18,9 +20,10 @@ for (var i = 0; i < images.length; i++) {
 }
 
 // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-modal.onclick = function() {
+
+span.onclick = function() {
   modal.style.display = "none";
 } 
